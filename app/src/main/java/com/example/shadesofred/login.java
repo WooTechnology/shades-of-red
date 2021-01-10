ee9a66da613b2f20671c41c2bb1ec9ead1ae7ef0
Madhuvanthi V <madhuvanthivenkat@gmail.com>
2021-01-10 17:09:14 +0530

    Changes made in login and sign up pages.

--- app/src/main/java/com/example/shadesofred/login.java
+++ app/src/main/java/com/example/shadesofred/login.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class login extends AppCompatActivity {
     public TextView tv_reg;
+    public Button btn_log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +18,19 @@ public class login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         tv_reg = (TextView)findViewById(R.id.tv_signup);
+        btn_log = (Button) findViewById(R.id.button);
 
         tv_reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(login.this, signup.class);
                 startActivity(intent);
+            }
+        });
+        btn_log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, MainActivity.class);
             }
         });
 
--- app/src/main/java/com/example/shadesofred/signup.java
+++ app/src/main/java/com/example/shadesofred/signup.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class signup extends AppCompatActivity {
     public TextView tv_login;
+    public Button btn_reg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,7 @@ public class signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         tv_login = (TextView)findViewById(R.id.tv_log);
+        btn_reg = (Button) findViewById(R.id.reg);
 
         tv_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +27,14 @@ public class signup extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btn_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signup.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
