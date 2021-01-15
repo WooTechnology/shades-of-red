9b04ef53f6d5fab9dad2a1342be3d95dfe0d0053
Akanksha Pant <pantakshi1999@gmail.com>
2021-01-15 13:14:32 +0530

    Donor list modified

--- app/src/main/java/com/example/shadesofred/donarCard.java
+++ app/src/main/java/com/example/shadesofred/donarCard.java
@@ -2,14 +2,33 @@ package com.example.shadesofred;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class donarCard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_donar_card);
+        setContentView(R.layout.rough_donorlist);
         getSupportActionBar().hide();
+    }
+    public void callDonor(View view){
+        Uri number = Uri.parse("9967940321");
+        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+        startActivity(callIntent);
+    }
+    public void callDonor1(View view){
+        Uri number = Uri.parse("9978940628");
+        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+        startActivity(callIntent);
+    }
+
+    public void  sendMessage(View view){
+        Uri number = Uri.parse("9967940321");
+        Intent sentIntent = new Intent(Intent.ACTION_SENDTO,number );
+        startActivity(sentIntent);
     }
 }
