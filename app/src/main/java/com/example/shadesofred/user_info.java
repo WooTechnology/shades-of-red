c61d27d704682aac370a1ca1977520f51fa3080c
priyal bhatewara <47683250+priyalbhatewara123@users.noreply.github.com>
2021-01-24 17:53:25 +0530

    Changed UserProfile

--- app/src/main/java/com/example/shadesofred/user_info.java
+++ app/src/main/java/com/example/shadesofred/user_info.java
@@ -8,22 +8,19 @@ import android.view.View;
 import android.widget.Button;
 
 public class user_info extends AppCompatActivity {
-    public Button edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Profile");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_user_info);
-        getSupportActionBar().hide();
 
-    edit = (Button) findViewById(R.id.btn_edit);
-
-        edit.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(user_info.this, userInfoEdit.class);
-            startActivity(intent);
-        }
-    });
 }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
 }
